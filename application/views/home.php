9f93fdf199ce183c55dd427bc594028955560c96
edysaputra66 <161240000502@unisnu.ac.id>
2019-10-17 14:01:38 +0700

    edit view home

--- application/views/home.php
+++ application/views/home.php
@@ -24,7 +24,7 @@
                         <div class="inner">
                             <h3>150</h3>
 
-                            <p>New Orders</p>
+                            <p>Pemesanan Tiket Bus</p>
                         </div>
                         <div class="icon">
                             <i class="ion ion-bag"></i>
