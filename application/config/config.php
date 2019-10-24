c9f90bfddfa32fe33a4ecfef56d5e5192cf3c42f
edysaputra66 <161240000502@unisnu.ac.id>
2019-10-24 14:46:19 +0700

    edit config

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,8 @@ defined('BASEPATH') or exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://localhost/e-tiket-bus';
+$config['base_url'] = 'http://localhost/PWFL_UNISNU_DB_e-tiket-bus
+/';
 
 /*
 |--------------------------------------------------------------------------
